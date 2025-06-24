86187b01a67e95d0c0b51f47ebbb19072b914809
Tarun Varshney <156907766+tarun6546@users.noreply.github.com>
2025-06-24 12:58:23 +0530

    Delete PracticeProblems/Assignment 1 directory

--- PracticeProblems/Assignment 1/Main4.java
+++
@@ -1,9 +0,0 @@
-//Area of a Circle
-import java.util.*;
-public class Main4{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		float radius=sc.nextFloat();
-		System.out.println(3.14*radius*radius);	
-	}
-}
