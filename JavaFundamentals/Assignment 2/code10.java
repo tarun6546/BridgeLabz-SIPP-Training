fa990843c921f6ce234326c6abb1aa90186c3d41
tarun6546 <tarunvarshney2112@gmail.com>
2025-06-30 22:22:49 +0530

    [Tarun Varshney].Add Problems For class and object on Day 10

--- JavaFundamentals/Assignment 2/code10.java
+++
@@ -1,7 +0,0 @@
-public class code10 {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers / 1.6;
-        System.out.printf("The distance %.1f km in miles is %.2f\n", kilometers, miles);
-    }
-}
--- PracticeProblems/Assignment 1/Main1.java
+++
@@ -1,6 +0,0 @@
-//Welcome to Bridgelabz!
-public class Main1{
-	public static void main(String[] args){
-		System.out.println("Welcome to Bridgelabz!");
-	}
-}
